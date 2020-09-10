7d0811fb8e1368de1af2ca0435e5b91337c53d93
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-09-10 14:12:08 +0300

    Account Service (Data Service / Provider) Added

--- src/app/shared/logging.service.ts
+++ src/app/shared/logging.service.ts
@@ -1,4 +1,9 @@
 export class LoggingService {
+
+    logAccountAdded(account: string){
+        console.log("A server added, new server: " + account)
+    }
+
     logStatusChange(status: string) {
         console.log('A server status changed, new status: ' + status);
     }
